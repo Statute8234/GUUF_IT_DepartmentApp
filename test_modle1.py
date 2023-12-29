4095278a0ff6e938533530700948a317a9e9f8d5
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2023-12-29 15:11:37 -0500

    Update test_modle1.py

--- test_modle1.py
+++ test_modle1.py
@@ -18,7 +18,7 @@ conn.close()"""
 
 import sqlite3
 
-def clear_table():
+"""def clear_table():
     conn = sqlite3.connect('employee.db')  # Replace 'your_database.db' with the actual name of your SQLite database
     cursor = conn.cursor()
 
@@ -31,4 +31,27 @@ def clear_table():
     finally:
         conn.close()
 
-clear_table()
+clear_table()"""
+
+import requests
+import sqlite3
+
+local_database = sqlite3.connect('employee.db')
+
+def sync_data():
+    # Retrieve local changes
+    cursor = local_database.cursor()
+    cursor.execute('SELECT * FROM employee WHERE synced = 0')
+    local_changes = cursor.fetchall()
+
+    # Synchronize with the central database
+    try:
+        response = requests.post('http://your-api-endpoint/sync', json=local_changes)
+        if response.json()['status'] == 'success':
+            # Update local changes as synced
+            cursor.execute('UPDATE employee SET synced = 1 WHERE synced = 0')
+            local_database.commit()
+    except Exception as e:
+        print(f'Synchronization error: {e}')
+
+sync_data()
